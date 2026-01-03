21a5886f39c4597523a2d0eabdcb0c8b29318c48
Hst332 <hst332@gmail.com>
2026-01-03 19:33:05 +0100

    Delete .github/regime_adjustment.py

--- .github/regime_adjustment.py
+++
@@ -1,9 +0,0 @@
-def adjust_metals_for_regime(metals, regime):
-    for m in metals:
-        if regime == "Recession" and m["commodity"] in ["Silver", "Copper"]:
-            m["bull"] -= 5
-            m["bear"] += 5
-        if regime == "Stagflation" and m["commodity"] == "Gold":
-            m["bull"] += 5
-            m["bear"] -= 5
-    return metals
